05f8ad6742c3942fdf788f5548b5ef8b632374d1
manish9427 <manish119427@gmail.com>
2023-10-01 10:23:12 +0530

    new

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,12 @@ import Header from "./components/header/Header";
 import Footer from "./components/footer/Footer";
 import Home from "./pages/home/Home";
 
-import { BrowserRouter } from "react-router-dom";
-
 function App() {
   return (
     <div>
-      <BrowserRouter>
-        <Header />
-        <Home />
-        <Footer />
-      </BrowserRouter>
+      <Header />
+      <Home />
+      <Footer />
     </div>
   );
 }
