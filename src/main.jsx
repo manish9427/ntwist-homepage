4de779919f7aa37bd17969ee194163faf00e232f
manish9427 <manish119427@gmail.com>
2023-10-02 18:27:14 +0530

    new

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,5 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.scss";
-import { store } from "./store/store.js";
-import { Provider } from "react-redux";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <Provider store={store}>
-    <App />
-  </Provider>
-);
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
