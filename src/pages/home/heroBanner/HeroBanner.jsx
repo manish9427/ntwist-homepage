537707630d246771be003d84799afec4e4d7d0b5
manish9427 <manish119427@gmail.com>
2023-10-02 18:37:26 +0530

    new

--- src/pages/home/heroBanner/HeroBanner.jsx
+++ src/pages/home/heroBanner/HeroBanner.jsx
@@ -1,14 +1,13 @@
 import React from "react";
 import "./style.scss";
 
-import Img from "../../../components/lazyLoadImage/Img";
 import ContentWrapper from "../../../components/contentWrapper/ContentWrapper";
 
 const HeroBanner = () => {
   return (
     <div className="heroBanner">
       <div className="backdrop-img">
-        <Img src="https://ntwist.com/wp-content/uploads/2023/01/home-image-bg-v6.png" />
+        <img src="https://ntwist.com/wp-content/uploads/2023/01/home-image-bg-v6.png" />
       </div>
 
       <div className="opacity-layer"></div>
